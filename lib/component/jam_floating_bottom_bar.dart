29c641954fd80654b123fe637706c76347d13a6b
Oralita <oat431@gmail.com>
2021-04-13 17:37:30 +0700

    create topbar and bottombar

---
+++ lib/component/jam_floating_bottom_bar.dart
@@ -0,0 +1,64 @@
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
+import 'package:jam_music_platform/screen/jam_profile_screen.dart';
+import 'package:jam_music_platform/screen/jam_main_screen.dart';
+import 'package:flutter/material.dart';
+
+class JamBottomBar extends StatefulWidget {
+  @override
+  _JamBottomBarState createState() => _JamBottomBarState();
+}
+
+class _JamBottomBarState extends State<JamBottomBar> {
+  int _index = 0;
+  _checkScreen() {
+    switch (_index) {
+      case 0:
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) => JamMainScreen(),
+          ),
+          (route) => false,
+        );
+        break;
+      case 1:
+        print('something');
+        break;
+      case 2:
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) =>
+                JamProfileScreen(name: 'Sahachan'),
+          ),
+          (route) => false,
+        );
+        break;
+      default:
+        print('no such a index');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingNavbar(
+      onTap: (int index) => {setState(() => _index = index), _checkScreen()},
+      currentIndex: _index,
+      items: [
+        FloatingNavbarItem(icon: Icons.home_rounded, title: ''),
+        FloatingNavbarItem(icon: Icons.add_circle_outline, title: ''),
+        FloatingNavbarItem(icon: Icons.person_outline_rounded, title: '')
+      ],
+      fontSize: 1,
+      backgroundColor: Colors.white,
+      borderRadius: 30,
+      unselectedItemColor: Colors.black,
+      selectedItemColor: Colors.black,
+      width: 356,
+      iconSize: 36,
+      padding: EdgeInsets.only(top: 2, bottom: 2),
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+    );
+  }
+}
--- lib/component/jam_top_bar.dart
+++ lib/component/jam_top_bar.dart
@@ -1,16 +1,48 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 class JamTopBar extends StatelessWidget implements PreferredSizeWidget {
+  String title;
+  String leading;
+  Function function;
+  JamTopBar({this.title, this.leading, this.function});
   @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: Text('Jam Music'),
+  Size get preferredSize => const Size.fromHeight(50);
+
+  Widget _imageBox(String name) {
+    return Image.asset(
+      name,
+      scale: 2,
+    );
+  }
+
+  Widget _actionButton(String icon) {
+    return GestureDetector(
+      onTap: () => {},
+      child: _imageBox(icon),
     );
   }
 
   @override
-  // TODO: implement preferredSize
-  Size get preferredSize => throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: this.function,
+        child: _imageBox(leading),
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      actions: [
+        _actionButton('assets/icon/Send_0.png'),
+        _actionButton('assets/icon/Notification_0.png'),
+      ],
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0,
+    );
+  }
 }

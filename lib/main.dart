8ac2dcc6feee5c99d513bbd7e5ac841f49e75b70
Oralita <oat431@gmail.com>
2021-04-13 17:39:21 +0700

    make the app functionable

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:jam_music_platform/screen/jam_jam_camera_screen.dart';
+import 'package:jam_music_platform/screen/jam_jam_music_screen.dart';
+import 'package:jam_music_platform/screen/jam_main_screen.dart';
+import 'package:jam_music_platform/screen/jam_profile_screen.dart';
+
+void main() => runApp(MainApp());
 
 class MainApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    JamMainScreen.tag: (context) => JamMainScreen(),
+    JamMusicScreen.tag: (context) => JamMusicScreen(),
+    JamCameraScreen.tag: (context) => JamCameraScreen(),
+    JamProfileScreen.tag: (context) => JamProfileScreen(),
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Jam Music app",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Jam music'),
-        ),
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        fontFamily: 'SFProText',
       ),
+      title: "Jam Music app",
+      home: JamMainScreen(),
+      routes: routes,
     );
   }
 }
